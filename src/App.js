1cfd65140b7fe5adaf3263ec0ce3c31e8a4c51e3
andgarzonmal <andgarzonmal@unal.edu.co>
2022-06-29 20:16:27 -0500

    check how JSX works

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name = "Andres"
+  let auth = true;
+  let estaciones = ["primavera","verano","otoño","invierno"]
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>My Name is {name}</h1>
+        <p>{auth ? "El usuario esta logueado" : "El usuario No esta logueado"}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul>
+          {estaciones.map((el, i) => 
+           <li key={i}>{el}</li> )}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
